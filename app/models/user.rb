e0822ec0c1d9c7b9fb587dead9355a45d0ff540b
Jeko0 <japulava@unisens.ge>
2022-07-19 11:30:07 +0400

    added vip role

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   
-  enum role: [:user, :admin]
+  enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
