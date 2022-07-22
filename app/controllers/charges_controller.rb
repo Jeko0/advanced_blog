1df088f155b54de9228df6dffdd3dbe404ed4f40
Jeko0 <japulava@unisens.ge>
2022-07-22 11:30:14 +0400

    saving payment id

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,28 @@
 class ChargesController < ApplicationController
   def new
+    @user = User.find_by(id: params[:user_id])
   end
-  
+
   def create
-  @amount = 1000
-  
-  customer = Stripe::Customer.create(
-  :email => params[:stripeEmail],
-  :source => params[:stripeToken]
-  )
-  
-  charge = Stripe::Charge.create(
-  :customer => customer.id,
-  :amount => @amount,
-  :description => "Rails Stripe customer",
-  :currency => "usd"
-  )
-  
-  rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
+    @amount = 1000
+
+    customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+    :customer => customer.id,
+    :amount => @amount,
+    :description => 'Rails Stripe customer',
+    :currency => "usd"
+    )
+    
+    @user = User.find_by(id: params[:user_id])
+    @user.update(role: 1)
+
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
   end
 end
