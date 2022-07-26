3bb90863746e1a835f634c8a61a4f232cbf6a9e8
Jeko0 <japulava@unisens.ge>
2022-07-26 15:01:05 +0400

    removed logic from view to decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,26 +2,37 @@ class UserDecorator < ApplicationDecorator
   include Draper::LazyHelpers
   delegate_all 
 
+  def edit_profile
+    if self == current_user 
+      link_to t(:edit), edit_user_registration_path, class:"btn btn-warning"
+    end
+  end
 
+  def profile_picture 
+    self.avatar? ? image_tag(self.avatar_url) : image_tag("download.png")
+  end
+
+  def add_phone
+    valid_user(link_to "Add Phone", add_phone_path, class:"btn btn-warning")
+  end
+
+  def add_location 
+    valid_user(link_to "Add location", add_location_path, class:"btn btn-warning")
+  end
+
+  def become_vip 
+    valid_user(render "charges/new", user: current_user)
+  end
 
   def first_address
     address = user.locations
-    address.first ? address.first.address : translate("no_address") 
+    address.first ? address.first.address : "no address"
   end
 
-  def first_3address
-    self.locations.first(3)
+  def first_two_address
+    self.locations.first(2)
   end
 
-  def first_3numbers
-    self.mobiles.first(3)
-  end
-
-  def add_address(current_user)
-    if is_user_current_user?(current_user)
-      link_to translate("add_address"), add_location_path
-    end
-  end
   
   def delete_address(current_user, address_id)
     if is_user_admin_or_current_user?(current_user)
@@ -40,12 +51,17 @@ class UserDecorator < ApplicationDecorator
     } 
   end
   private
+    def is_user_current_user?(current_user)
+      self == current_user
+    end
 
-  def is_user_current_user?(current_user)
-    self == current_user
-  end
+    def is_user_admin_or_current_user?(current_user)
+      current_user.admin? || self == current_user 
+    end
 
-  def is_user_admin_or_current_user?(current_user)
-    current_user.admin? || self == current_user 
-  end
+    def valid_user(param)
+      if current_user == self 
+        param
+      end
+    end
 end
