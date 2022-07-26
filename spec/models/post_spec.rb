338ddbbb3654609971994972d2c0656e616d627e
Jeko0 <japulava@unisens.ge>
2022-07-26 15:31:44 +0400

    tested post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.create(email: 'example@new.com', password: 'password', password_confirmation: 'password') }
+  subject(:post) { described_class.new(title: 'title', body: 'content', user_id: user.id, approved: true) }
+
+  context "Associations" do
+    it { should belong_to(:user) }
+    it { should have_many(:comments) }
+    it { should have_many(:likes) }
+  end
+
+  context "Validations" do 
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
 end
