148b0578d620fc20aa0cb191f747194dbb79ad5c
Jeko0 <japulava@unisens.ge>
2022-08-01 16:04:28 +0400

    tested home controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe HomeController, type: :controller do
     subject { get :index } 
     it { should have_http_status(:success) }
   end
+
+  context "profile" do 
+    it "routes to #profile" do
+      expect(get: "/user_profile/:id").to route_to({
+        controller: "home",
+        action: "profile",
+        id: ":id"
+      })
+    end
+  end
 end
